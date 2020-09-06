56f3d432327e7e7fa0453271707910f8fc813623
Kyle Icban <kyleicban@yahoo.com>
2020-09-05 21:11:18 -0700

    added PageWrapper.js and WorkExpMaker.js and started the Work Experience page

---
+++ src/components/PageWrapper.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const PageWrapper = styled.div`
+  background: white;
+  border-radius: 10px;
+  margin: 50px;
+  padding: 10px;
+  position: relative;
+  text-align: center;
+  top: -75px;
+  z-index: 1;
+
+  -moz-box-shadow: 0px 15px 50px 5px #ccc;
+  -webkit-box-shadow: 0px 15px 50px 5px #ccc;
+  box-shadow: 0px 15px 50px 5px rgb(5, 5, 59);
+`;
+
+export const TitleWrapper = styled.p`
+  color: rgb(15, 8, 43);
+  font-size: 50px;
+  text-decoration: underline;
+`;
--- src/pages/WorkExperience.js
+++ src/pages/WorkExperience.js
@@ -1,14 +1,62 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
+import { PageWrapper, TitleWrapper } from "../components/PageWrapper";
+
 const WorkExpWrapper = styled.div`
-  background: white;
+  color: rgb(15, 8, 43);
+  margin: auto;
+  padding-bottom: 40px;
+  padding: 40px;
+  width: 600px;
+`;
+
+const TestWork = styled.a`
+  background: red;
+  color: rgb(15, 8, 43);
+  margin: auto;
+  margin-bottom: 40px;
+  position: absolute;
+  width: 100%;
 `;
 
 export default class WorkExp extends Component {
   render() {
     //====EVENTUALLY WANT IT SO I PASS IN AN OBJECT====
     //====AND IT FILLS IN EVERYTHING====
-    return <WorkExpWrapper>I worked at {this.props.company}</WorkExpWrapper>;
+    return (
+      <PageWrapper>
+        <TitleWrapper>WORK EXPERIENCE</TitleWrapper>
+        <WorkExpWrapper>
+          <div className="card m-1 p-1">
+            <TestWork
+              target="_blank"
+              href="https://www.google.com/webhp?authuser=1"
+              className="btn btn-light"
+            >
+              Google
+            </TestWork>
+          </div>
+          <div className="card m-1 p-1">
+            <TestWork
+              target="_blank"
+              href="https://www.google.com/webhp?authuser=1"
+              className="btn btn-light"
+            >
+              Google
+            </TestWork>
+          </div>
+          <div className="card m-1 p-1">
+            <TestWork
+              target="_blank"
+              href="https://www.google.com/webhp?authuser=1"
+              className="btn btn-light"
+            >
+              Google
+            </TestWork>
+          </div>
+        </WorkExpWrapper>
+      </PageWrapper>
+    );
   }
 }
