702503fa473b0fb1a024b446bed36780adf545e5
Kyle Icban <kicban29@gmail.com>
2022-03-01 19:28:11 -0800

    smallerrrrr

--- src/shared/LineBreak.js
+++ src/shared/LineBreak.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
 export const LineBreak = styled.div`
-    background: rgba(15, 33, 56, 0.5);
+    background: rgba(15, 8, 43, 0.5);
     border-radius: 10px;
-    height: 2px;
+    height: 0.5px;
     margin: auto;
     margin-bottom: 50px;
     width: 80%;
